4f03a8b4c13334f50f96171b7162dd3cac204257
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2021-10-11 05:47:39 +0000

    i have changed the code .

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # display Fibonacci series
+d = {}
 def Fibonacci(n):
+  if(n in d):
+    return d[n]
   if (n <= 1):
     return(n)
   else:
-    return(Fibonacci(n-1) + Fibonacci(n-2))
+    variableValue = (Fibonacci(n-1) + Fibonacci(n-2))
+    d[n]=variableValue
+    return(variableValue)
 # user input
-n = int(input("enter a number :"))
-
+n = int(input("enter a number:"))
 if n <= 0:
   print("enter a positive number:")
 else:
   print("Fibonacci series is :")
-  for i in range(n):
-    print(Fibonacci(i))
-
-  
+  print(Fibonacci(n))
 
 
 
